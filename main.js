78459f0b037a14bb2ce4d7efd00fde68df01da90
jack ashton <jackdashton@outlook.com>
2023-06-06 21:54:25 +0100

    added quiz questions

--- main.js
+++ main.js
@@ -12,12 +12,13 @@ const moveForward = (event) => {
       car.nextElementSibling.classList.add("user");
     } else {
       alert("You have escaped, well done!");
+      window.location.reload();
     }
   } else if (event.key === "q") {
     const bird = document.querySelector(".owl");
     if (bird.classList.contains("user") && bird.classList.contains("owl")) {
       alert("Game over, the owl caught you");
-      bird.classList.remove("owl");
+      window.location.reload();
     } else
     bird.classList.remove("owl");
     bird.nextElementSibling.classList.add("owl");
@@ -26,3 +27,37 @@ const moveForward = (event) => {
 
 // Add an event listener on keyup which calls a function
 document.addEventListener("keyup", moveForward);
+
+// Questions
+// Array of objects to store questions and answers
+
+const myQuestions = [
+  {
+    question: "Haus",
+    correctAnswer: "House"
+  },
+  {
+    question: "Bier",
+    correctAnswer: "Beer"
+  },
+];
+
+// Function for the quiz
+
+// generateQuiz function contains helper functions to show the quiz, accept submissions, and show the results.
+
+const generateQuiz = (questions, quizContainer, resultsContainer, submitButton) => {
+  const showQuestions = (questions, quizContainer) => {
+
+  }
+  const showResults = (questions, quizContainer, resultsContainer) => {
+
+  }
+
+  showQuestions(questions, quizContainer);
+
+  submitButton.onclick = function() {
+    showResults(questions, quizContainer, resultsContainer)
+  }
+
+}
