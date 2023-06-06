7c6ac2ddc31ff4d50a69938a76b0c4698fb3b1e5
jack ashton <jackdashton@outlook.com>
2023-06-06 21:15:45 +0100

    implemented car movement js file

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("Connected");
+
+// player one - Have user press a key
+// when suer presses key, check to see if there is an available sibling
+// if yes, move active class from current to next
+// if no available td, alert winner
+
+// We pass 1 or 2 through as the arguement (i.e. player)
+
+// Add event listener to keypress
+
+const moveForward = (event) => {
+  if (event.key === "p") {
+    const car = document.querySelector(".user");
+    console.log(car);
+    car.classList.remove("user");
+    car.nextElementSibling.classList.add("user");
+  }
+};
+
+document.addEventListener("keyup", moveForward);
