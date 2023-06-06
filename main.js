60b9e3eae44bcaeb1d44ea0e748eb7bf0eac117d
jack ashton <jackdashton@outlook.com>
2023-06-06 21:28:26 +0100

    game over and finish functions added

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
-console.log("Connected");
-
-// player one - Have user press a key
 // when suer presses key, check to see if there is an available sibling
 // if yes, move active class from current to next
 // if no available td, alert winner
 
 // We pass 1 or 2 through as the arguement (i.e. player)
 
-// Add event listener to keypress
-
 const moveForward = (event) => {
   if (event.key === "p") {
     const car = document.querySelector(".user");
-    console.log(car);
-    car.classList.remove("user");
-    car.nextElementSibling.classList.add("user");
+    if (car.nextElementSibling) {
+      car.classList.remove("user");
+      car.nextElementSibling.classList.add("user");
+    } else {
+      alert("You have escaped, well done!");
+    }
   } else if (event.key === "q") {
     const bird = document.querySelector(".owl");
+    if (bird.classList.contains("user") && bird.classList.contains("owl")) {
+      alert("Game over, the owl caught you");
+      bird.classList.remove("owl");
+    } else
     bird.classList.remove("owl");
     bird.nextElementSibling.classList.add("owl");
   }
 };
 
+// Add an event listener on keyup which calls a function
 document.addEventListener("keyup", moveForward);
