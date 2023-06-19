9a122c4054f78330983217330050f7c078def93e
jack ashton <jackdashton@outlook.com>
2023-06-19 14:38:43 +0100

    quiz js finished

--- main.js
+++ main.js
@@ -29,10 +29,9 @@ let start = true;
 // Iteration
 
 function iterate(id) {
-
-  // Getting the results display section
-  const results = document.querySelector(".result");
-  results[0].innerText = "";
+  // Getting the result display section
+  const result = document.querySelectorAll(".result");
+  result[0].innerText = "";
 
   // Getting the question
 
@@ -49,35 +48,73 @@ function iterate(id) {
 
   // Providing Option Text
   option1.innerText = Questions[id].a[0].text;
-  option1.innerText = Questions[id].a[1].text;
-  option1.innerText = Questions[id].a[2].text;
-  option1.innerText = Questions[id].a[3].text;
+  option2.innerText = Questions[id].a[1].text;
+  option3.innerText = Questions[id].a[2].text;
+  option4.innerText = Questions[id].a[3].text;
 
   // True of false value to the options
+  option1.value = Questions[id].a[0].isCorrect;
+  option2.value = Questions[id].a[1].isCorrect;
+  option3.value = Questions[id].a[2].isCorrect;
+  option4.value = Questions[id].a[3].isCorrect;
 
+  let selected = "";
 
+  // Show selection for op1
+  option1.addEventListener("click", () => {
+    option1.computedStyleMap.backgroundColor = "lightgoldenrodyellow";
+    selected = option1.value;
+  });
 
+  // Show selection for op2
+  option2.addEventListener("click", () => {
+    option2.computedStyleMap.backgroundColor = "lightgoldenrodyellow";
+    selected = option2.value;
+  });
 
+  // Show selection for op3
+  option3.addEventListener("click", () => {
+    option3.computedStyleMap.backgroundColor = "lightgoldenrodyellow";
+    selected = option3.value;
+  });
 
+  // Show selection for op4
+  option4.addEventListener("click", () => {
+    option4.computedStyleMap.backgroundColor = "lightgoldenrodyellow";
+    selected = option4.value;
+  });
+
+  // Get evaluate button
+  const evaluate = document.querySelector(".evaluate");
+
+  // Evaluate Method
+  evaluate.addEventListener("click", () => {
+    if (selected == "true") {
+      // add in function to move user
+      result[0].innerHTML = "True";
+    } else {
+      // add in function to move owl
+      result[0].innerHTML = "False";
+    }
+  });
 }
 
-const results = document.querySelector(".result")
-  console.log(results)
+if (start) {
+  iterate("0");
+}
 
+// Next Button
+const next = document.querySelector(".next");
+let id = 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+next.addEventListener("click", () => {
+  start = false;
+  if (id < 2) {
+    id++;
+    iterate(id);
+    console.log(id);
+  }
+});
 
 // Racetrack
 
